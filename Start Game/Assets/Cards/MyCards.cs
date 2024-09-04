b61798299e4d30891900744441ee7bf08b799580
AndroidO9 <dozmorovk50@gmail.com>
2024-09-04 20:46:09 +0500

    Razdacha Kards

---
+++ Start Game/Assets/Cards/MyCards.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCards : MonoBehaviour
+{
+    public List<GameObject> MCards;
+	private void Update()
+	{
+		MCards.Clear();
+		foreach(Transform t in transform)
+		{
+			MCards.Add(t.gameObject);
+		}
+		gameObject.transform.position = new Vector3((2.1f * MCards.Count)/2, 1f, -5f);
+		for (int i = 0; i < MCards.Count; i++)
+		{
+			MCards[i].transform.position = new Vector3(2.1f * i-transform.position.x+1,2,-5);
+		}
+	}
+}
