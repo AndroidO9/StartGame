817ce96a4654219d8ea0fd5aa3412a2e86d2805d
AndroidO9 <dozmorovk50@gmail.com>
2024-09-05 22:22:02 +0500

    Revert "GGG"
    This reverts commit 57c6cd8880edd08fcb5c5d01195c9d19450c1c29.

--- Start Game/Assets/Cards/MyCards.cs
+++ Start Game/Assets/Cards/MyCards.cs
@@ -12,10 +12,10 @@ public class MyCards : MonoBehaviour
 		{
 			MCards.Add(t.gameObject);
 		}
-		gameObject.transform.position = new Vector3((2.1f * MCards.Count)/2, 1f, gameObject.transform.position.z);
+		gameObject.transform.position = new Vector3((2.1f * MCards.Count)/2, 1f, -5f);
 		for (int i = 0; i < MCards.Count; i++)
 		{
-			MCards[i].transform.position = new Vector3(2.1f * i-transform.position.x+1,1, gameObject.transform.position.z);
+			MCards[i].transform.position = new Vector3(2.1f * i-transform.position.x+1,2,-5);
 		}
 	}
 }
