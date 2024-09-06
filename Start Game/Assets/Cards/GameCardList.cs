08ced9a71df79ab1df8f287c76d7ade0d90a6867
Mazing21 <makszadorin0@gmail.com>
2024-09-06 18:26:10 +0500

    CardList

---
+++ Start Game/Assets/Cards/GameCardList.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+using UnityEngine;
+public class GameCardList : MonoBehaviour
+{
+    [SerializeField] public List<CardScriptableObject> GameCards = new List<CardScriptableObject>();
+}
